393fa1f1919ca1568189c6342002756c812d5d01
T.agd <technically.a.game.dev@gmail.com>
2018-12-04 21:32:59 +0800

    Wrote tests for unpack

--- src/unpack.nim
+++ src/unpack.nim
@@ -1,65 +1,48 @@
 import macros
 
-proc unpackInternal(srcNode,args:NimNode;sec,statement:NimNodeKind): NimNode = 
+proc unpackInternal(srcNode, dests: NimNode; sec,
+  statement: NimNodeKind): NimNode =
   result = newStmtList()
   var section = sec.newTree()
-  var stNode = statement.newTree(newEmptyNode(),newEmptyNode())
+  var stNode = statement.newTree(newEmptyNode(), newEmptyNode())
   if statement != nnkAsgn:
     stNode.add(newEmptyNode())
   var src = srcNode
+  # Creates a temporary symbol to store proc result.
   if src.kind != nnkSym:
-    src = genSym(nskLet,"src")
-    result.add(newLetStmt(src,srcNode))
+    src = genSym(nskLet, "src")
+    result.add(newLetStmt(src, srcNode))
   let typ = srcNode.getType
   case typ.typeKind:
-  of ntySequence,ntyArray,ntyOpenArray:
+  of ntySequence, ntyArray, ntyOpenArray, ntyTuple:
     var i = 1
-    for arg in args.children:
+    for dest in dests.children:
       var newNode = stNode.copyNimTree
-      newNode[0] = arg
-      newNode[^1] = nnkBracketExpr.newTree(src,newLit(i-1))
+      newNode[0] = dest
+      newNode[^1] = nnkBracketExpr.newTree(src, newLit(i-1))
       section.add(newNode)
       inc i
-  of ntyObject,ntyRef,ntyPtr:
-    for arg in args.children:
-      case arg.kind:
+  of ntyObject, ntyRef, ntyPtr:
+    for dest in dests.children:
+      case dest.kind:
       of nnkExprEqExpr:
         var newNode = stNode.copyNimTree
-        newNode[0] = arg[1]
-        newNode[^1] = nnkDotExpr.newTree(src,arg[0])
+        newNode[0] = dest[1]
+        newNode[^1] = nnkDotExpr.newTree(src, dest[0])
         section.add(newNode)
       else:
         var newNode = stNode.copyNimTree
-        newNode[0] = arg
-        newNode[^1] = nnkDotExpr.newTree(src,arg)
+        newNode[0] = dest
+        newNode[^1] = nnkDotExpr.newTree(src, dest)
         section.add(newNode)
-  else: error("Oh NOOoo! Type `" & $src.typeKind & "` can not be unpacked!",src)
+  else: error("Oh NOOoo! Type `" & $src.typeKind & "` can not be unpacked!",
+      src)
   result.add(section)
 
-macro vunpack*(src:typed;args: varargs[untyped]): typed =
-  unpackInternal(src,args,nnkVarSection,nnkIdentDefs)
-macro lunpack*(src:typed; args: varargs[untyped]): typed =
-  unpackInternal(src,args,nnkLetSection,nnkIdentDefs)
-macro unpack*(src:typed; args: varargs[typed]): typed =
-  unpackInternal(src,args,nnkStmtList,nnkAsgn)
+macro vunpack*(src: typed; dests: varargs[untyped]): typed =
+  unpackInternal(src, dests, nnkVarSection, nnkIdentDefs)
+macro lunpack*(src: typed; dests: varargs[untyped]): typed =
+  unpackInternal(src, dests, nnkLetSection, nnkIdentDefs)
+macro unpack*(src: typed; dests: varargs[typed]): typed =
+  unpackInternal(src, dests, nnkStmtList, nnkAsgn)
 
-
-when isMainModule:
-  let lol = @[1,2,3]
-  var g,q:int
-  lol.unpack(g,q)
-  echo g,q
-  type
-    Person = object
-      name,job:string
-  proc collegue(p:Person;name:string):Person =  
-    result = p
-    result.name = name
-  let tim = Person(name:"Tim",job:"Fluffer")
-  tim.vunpack(name,job)
-  echo name
-  echo job
-  echo "===New Guy==="
-  tim.collegue("John").unpack(name,job)
-  echo name
-  echo job
--- tests/test1.nim
+++ tests/test1.nim
@@ -1,12 +1,98 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-
 import unittest
 
 import unpack
-test "can add":
-  check add(5, 5) == 10
+
+suite "Sequence unpacking":
+  setup:
+    let testSeq = @[2, 4, 6]
+    let testArray = [2, 4, 6]
+    let testTuple = (2, 4, 6)
+
+  test "should unpack sequence":
+    testSeq.lunpack(a, b, c)
+    check [a, c, b] == [2, 6, 4]
+    # is expanded into:
+    # let
+    #   a = testSeq[0]
+    #   b = testSeq[1]
+    #   c = testSeq[2]
+
+  test "should unpack array":
+    testArray.lunpack(a, b, c)
+    check [a, c, b] == [2, 6, 4]
+  test "should unpack tuple":
+    testTuple.lunpack(a, b, c)
+    check [a, c, b] == [2, 6, 4]
+    ## testTuple.lunpack(d, e, f, g) <- will cause IndexError at runtime
+  test "should unpack from index 0 to arbitrary number":
+    testTuple.lunpack(a, b)
+    check [a, b] == [2, 4]
+  test "vunpack should create variables with var":
+    testTuple.vunpack(a, b)
+    check [a, b] == [2, 4]
+    a = 13
+    check a == 13
+  test "lunpack defines symbols with let":
+    testTuple.lunpack(a, b)
+    check [a, b] == [2, 4]
+  test "unpack should assign data to existing variables":
+    var a, b = 1
+    testTuple.unpack(a, b)
+    check [a, b] == [2, 4]
+
+suite "Object meber unpacking":
+  type
+    Person = object
+      name, job: string
+    PersonRef = ref Person
+  setup:
+    let timName = "Tim"
+    let fluffer = "Fluffer"
+    let tim = Person(name: timName, job: fluffer)
+    let timRef = new(Person)
+    timRef.name = timName
+    timRef.job = fluffer
+    var secreteCounter = 0
+    proc colleague(p: Person; name: string): Person =
+      secreteCounter += 1
+      result = p
+      result.name = name
+  test "should unpack ordinary objects":
+    tim.lunpack(name, job)
+    check name == timName
+    check job == fluffer
+
+  test "should unpack object refs":
+    timRef.lunpack(name, job)
+    check name == timName
+    check job == fluffer
+
+  test "should unpack object pointers":
+    let timPtr = unsafeAddr(tim)
+    timPtr.lunpack(job, name)
+    check name == timName
+    check job == fluffer
+
+  test "should not call proc multiple times when invoked after a chain of calls":
+    let johnName = "John"
+    tim.colleague(johnName).lunpack(name, job)
+
+    # is expanded into:
+    # let someUniqueSym1212498 = tim.colleague(johnName)
+    # let
+    #   name = someUniqueSym1212498.name
+    #   job = someUniqueSym1212498.job
+
+    check name == johnName
+    check job == fluffer
+    check secreteCounter == 1
+
+  test "should be able to rename object member with '=' sign":
+    tim.lunpack(name = otherName)
+
+    check otherName == timName
+
+    tim.lunpack(job = otherJob, name = yetAnotherName) # and is order-agnostic.
+
+    check yetAnotherName == timName
+    check otherJob == fluffer
